6edd9876f3a12a778183a40d5638b0e295332a8a
GoshoPetrov <yankrustev@gmail.com>
2023-10-05 17:26:28 +0300

    Problem 5 - Special Numbers

---
+++ Special Numbers/Special Numbers/Special Numbers.cs
@@ -0,0 +1,39 @@
+﻿namespace Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int num = 0;
+            bool result = false;
+
+            for(int i = 1; i <= n; i++)
+            {
+                num = i;
+                while(true)
+                {
+                    sum += num%10;
+                    num = num / 10;
+                    if(num <= 0)
+                    {
+                        break;
+                    }
+                }
+
+                if(sum == 5 || sum == 7 || sum == 11)
+                {
+                    result = true;
+                }
+
+                Console.WriteLine($"{i} -> {result}");
+                
+                sum= 0;
+                result= false;
+                
+
+            }
+        }
+    }
+}
