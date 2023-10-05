24def4e4aaa27ef45eef8ada8ad59970b30cdd12
GoshoPetrov <yankrustev@gmail.com>
2023-10-05 16:38:27 +0300

    First 3 problems - Convent Metres to Kilometres, Exact Sum of Real Numbers, Pounds to Dollars

---
+++ Convert Meters to Kilometers/Convert Meters to Kilometers/Convert Meters to Kilometers.cs
@@ -0,0 +1,12 @@
+﻿namespace Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int metres = int.Parse(Console.ReadLine());
+            float kilometers = metres / 1000.0f;
+            Console.WriteLine($"{kilometers:F02}");
+        }
+    }
+}
---
+++ Pounds to Dollars/Pounds to Dollars/Pounds to Dollars.cs
@@ -0,0 +1,13 @@
+﻿namespace Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dolars = pounds * 1.31f;
+            Console.WriteLine($"{dolars:F03}");
+
+        }
+    }
+}
