b73b4b6ac5ce1d98f7e8fe6ff735af6718678310
GoshoPetrov <yankrustev@gmail.com>
2023-10-05 17:48:48 +0300

    Problem 6 - Reversed Chars

---
+++ Reversed Chars/Reversed Chars/Reversed Chars.cs
@@ -0,0 +1,13 @@
+﻿namespace Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char numOne = char.Parse(Console.ReadLine());
+            char numTwo = char.Parse(Console.ReadLine());
+            char numThree = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{numThree} {numTwo} {numOne}");
+        }
+    }
+}
