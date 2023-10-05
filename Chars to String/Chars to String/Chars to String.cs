11f1bb8e6ac383ef93f4b2341f7bcb7c2f85792d
GoshoPetrov <yankrustev@gmail.com>
2023-10-05 20:05:55 +0300

    Problem number 9, 10, 11

---
+++ Chars to String/Chars to String/Chars to String.cs
@@ -0,0 +1,16 @@
+﻿namespace Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char inputOne = char.Parse(Console.ReadLine());
+            char inputTwo = char.Parse(Console.ReadLine());
+            char inputThree = char.Parse(Console.ReadLine());
+
+            string sum = $"{inputOne}{inputTwo}{inputThree}";
+
+            Console.WriteLine(sum);
+}
+    }
+}
---
+++ Lower or Upper/Lower or Upper/Lower or Upper.cs
@@ -0,0 +1,20 @@
+﻿namespace Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if(char.IsUpper(letter))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if(char.IsLower(letter))
+            {
+                Console.WriteLine("lower-case");
+            }
+            
+        }
+    }
+}
