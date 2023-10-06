f78c49eedf1c07f74a34fab576ce7afbe6ea0f0e
GoshoPetrov <yankrustev@gmail.com>
2023-10-06 15:51:55 +0300

    Problem 5 - Print Part Of ASCII Table

---
+++ Print Part Of ASCII Table/Print Part Of ASCII Table/Print Part Of ASCII Table.cs
@@ -0,0 +1,20 @@
+﻿namespace Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStart = int.Parse(Console.ReadLine());
+            int numEnd = int.Parse(Console.ReadLine());
+
+            
+
+            for(int i = numStart; i <= numEnd;i++)
+            {
+                char x = (char)i;
+                Console.Write($"{x} ");
+
+            }
+        }
+    }
+}
