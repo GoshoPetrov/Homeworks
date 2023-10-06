43e81a3ca8fd7e1cbb28a6c1837496988e54f51f
GoshoPetrov <yankrustev@gmail.com>
2023-10-06 14:32:46 +0300

    Problem 3 - Elevator

---
+++ Elevator/Elevator/Elevator.cs
@@ -0,0 +1,19 @@
+﻿namespace Elevator
+{
+    internal class Elevator
+    {
+        static void Main(string[] args)
+        {
+            int nPeople = int.Parse(Console.ReadLine());
+            int pCapacity = int.Parse(Console.ReadLine());
+            int courses = 0;
+
+            for(int i = 0; i < nPeople; i+=pCapacity)
+            {
+                courses++;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}
