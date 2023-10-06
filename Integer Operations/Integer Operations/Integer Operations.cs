bf8821e6c4e4676a786c5122b22c129991cecb05
GoshoPetrov <yankrustev@gmail.com>
2023-10-06 13:28:45 +0300

    Homework Data Types and Variables - Problem 1 Integer Operations

---
+++ Integer Operations/Integer Operations/Integer Operations.cs
@@ -0,0 +1,20 @@
+﻿namespace Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numOne = decimal.Parse(Console.ReadLine());
+            decimal numTwo = decimal.Parse(Console.ReadLine());
+            decimal numThree = decimal.Parse(Console.ReadLine());
+            decimal numFour = decimal.Parse(Console.ReadLine());
+
+            decimal sum = numOne + numTwo;
+            decimal sumDivide = sum / numThree;
+            decimal sumAll = sumDivide * numFour;
+
+            Console.WriteLine(sumAll);
+            
+        }
+    }
+}
