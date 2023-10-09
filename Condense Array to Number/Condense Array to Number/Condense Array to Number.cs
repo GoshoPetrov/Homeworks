7e7dbb07358835c92d5d17a81badf1ef43f6e6e5
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 23:03:27 +0300

    Problem 7 - Condense Array to Number

---
+++ Condense Array to Number/Condense Array to Number/Condense Array to Number.cs
@@ -0,0 +1,24 @@
+﻿namespace Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while(num.Length > 1)
+            {
+                int[] n2 = new int[num.Length - 1];
+                for (int i = 0; i < num.Length - 1; i++)
+                {
+                    int e = num[i] + num[i + 1];
+                    n2[i] = e;
+                }
+
+                num = n2;
+            }
+
+            Console.WriteLine(num[0]);
+        }
+    }
+}
