a1867f61600431722f0601375df96982b4e25ed4
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 21:25:11 +0300

    Problem 2 - Print Numbers in Revers Order

---
+++ Print Numbers in Reverse Order/Print Numbers in Reverse Order/Print Numbers in Reverse Order.cs
@@ -0,0 +1,23 @@
+﻿namespace Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i =0; i < n; i++)
+            {
+                Console.Write($"{arr[n - 1 - i]} ");
+            }
+
+
+        }
+    }
+}
