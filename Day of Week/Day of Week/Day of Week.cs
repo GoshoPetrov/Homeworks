0b350759e39be8e6e5238aa7ef8ee0538cb092d6
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 21:02:17 +0300

    Homework - Problem 1

---
+++ Day of Week/Day of Week/Day of Week.cs
@@ -0,0 +1,21 @@
+﻿namespace Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int num = int.Parse(Console.ReadLine());
+
+            if(num >= 1 && num <= 7)
+            {
+                Console.WriteLine(day[num - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
