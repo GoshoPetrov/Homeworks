5dc5f998d53f6ca5bba81d7e005d0b74514b44eb
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 22:44:31 +0300

    Problem 5 - Sum Even Number

---
+++ Sum Even Numbers/Sum Even Numbers/Sum Even Numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            
+            foreach(int i in num)
+            {
+                if(i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
