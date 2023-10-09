aaf5091c853ceb2583ba818416b8d2143ee1dad4
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 22:50:22 +0300

    Problem 6 - Evem and Odd Substraction

---
+++ Even and Odd Subtraction/Even and Odd Subtraction/Even and Odd Subtraction.cs
@@ -0,0 +1,27 @@
+﻿namespace Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEven = 0;
+            int sumOdd = 0;
+            
+            foreach(int n in num)
+            {
+                if(n % 2 == 0)
+                {
+                    sumEven += n;
+                }
+                else
+                {
+                    sumOdd += n;
+                }
+            }
+            Console.WriteLine(sumEven - sumOdd);
+
+
+        }
+    }
+}
