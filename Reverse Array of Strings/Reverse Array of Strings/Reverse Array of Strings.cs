64d82200b33f044c8681df96e7b448824c5e6d01
GoshoPetrov <yankrustev@gmail.com>
2023-10-09 22:17:00 +0300

    Problem 4 - Reverse Array of Strings

---
+++ Reverse Array of Strings/Reverse Array of Strings/Reverse Array of Strings.cs
@@ -0,0 +1,25 @@
+﻿namespace Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+
+            for(int i = 0; i < arr.Length / 2; i++)
+            {
+                var x = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = x;
+                
+            }
+
+            foreach(var x in arr)
+            {
+                Console.Write($"{x} ");
+            }
+
+
+        }
+    }
+}
