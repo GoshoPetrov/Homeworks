209603e54bd336fd7d4ba8ff106304338a2a1222
GoshoPetrov <yankrustev@gmail.com>
2023-10-10 18:56:33 +0300

    Problem 2 - Zig-Zag Arrays

---
+++ Zig-Zag Arrays/Zig-Zag Arrays/Zig-Zag Arrays.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+namespace Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arrOne = new int[n];
+            int[] arrTwo = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine() // string
+                    .Split() // string[]
+                    .Select(int.Parse)
+                    .ToArray(); // int[]
+
+                if (i % 2 == 0)
+                {
+                    arrOne[i] = input[0];
+                    arrTwo[i] = input[1];
+                }
+                else
+                {
+
+                    arrOne[i] = input[1];
+                    arrTwo[i] = input[0];
+                }
+
+
+            }
+
+            Console.WriteLine(string.Join(" ", arrOne));
+            Console.WriteLine(string.Join(" ", arrTwo));
+
+            
+
+
+        }
+    }
+}
