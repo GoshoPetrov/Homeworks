3574872beb51e22e17feed7397a215a781283a1f
GoshoPetrov <yankrustev@gmail.com>
2023-10-10 19:19:31 +0300

    Problem 3 - Array Rotation

---
+++ Array Rotation/Array Rotation/Array Rotation.cs
@@ -0,0 +1,24 @@
+﻿namespace Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = arr[0];
+                for(int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length - 1] = x;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
