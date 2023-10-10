8d7e11d1f1441013e4f613f58632c8236406ddbb
GoshoPetrov <yankrustev@gmail.com>
2023-10-10 19:52:09 +0300

    Problem 4 - Top Integers

---
+++ Top Integers/Top Integers/Top Integers.cs
@@ -0,0 +1,30 @@
+﻿namespace Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for(int i = 0; i < arr.Length-1; i++)
+            {
+                bool greaterNot = true;
+                for(int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        greaterNot = false;
+                        break;
+                    }
+                }
+                if (greaterNot)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+            }
+
+            Console.Write(arr[arr.Length - 1]);
+
+        }
+    }
+}
