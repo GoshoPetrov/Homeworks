909febdd039957b6e8493da5fa93f128cf1d558b
GoshoPetrov <yankrustev@gmail.com>
2023-10-10 21:46:08 +0300

    Problem 5 - Equal Sum

---
+++ Equal Sum/Equal Sum/Equal Sum.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int n = arr[i];
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    rightSum += arr[j];
+                }
+
+                for(int x = 0; x < i; x++)
+                {
+                    leftSum += arr[x];
+                }
+
+                if(leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}
