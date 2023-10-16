7b3bd075979213207a67fc4fad540dd6d0e2e26c
GoshoPetrov <yankrustev@gmail.com>
2023-10-16 19:59:53 +0300

    Arrays exercises

---
+++ LadyBugs/LadyBugs/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] field = new int[size];
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            foreach (int index in indexes)
+            {
+                if (index < 0 || index >= size)
+                {
+                    continue;
+                }
+                field[index] = 1;
+            }
+
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] parts = command.Split();
+
+                int ladyBugIndex = int.Parse(parts[0]);
+                int flyLenght = int.Parse(parts[2]);
+
+                if (parts[1] == "left")
+                {
+                    flyLenght *= -1;
+                }
+
+                if (field[ladyBugIndex] == 0)
+                {
+                    continue;
+                }
+
+                field[ladyBugIndex] = 0;
+                do
+                {
+                    ladyBugIndex += flyLenght;
+                } while (ladyBugIndex >= 0 && ladyBugIndex < size && field[ladyBugIndex] == 1);
+
+                if (ladyBugIndex >= 0 && ladyBugIndex < size)
+                {
+                    field[ladyBugIndex] = 1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
---
+++ Max Sequence of Equal Elements/Max Sequence of Equal Elements/Max Sequence of Equal Elements.cs
@@ -0,0 +1,42 @@
+﻿namespace Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int bestCount = 0;
+            int bestN = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int n = arr[i];
+
+                int count = 1;
+                for (int j=i+1; j<arr.Length; j++)
+                {
+                    if (arr[j] != n)
+                    {
+                        break;
+                    }
+
+                    count++;
+                }
+
+                if (bestCount < count)
+                {
+                    bestCount = count;
+                    bestN = n;
+                }
+                
+            }
+
+            for (int i=0; i<bestCount; i++)
+            {
+                Console.Write($"{bestN} ");
+            }
+
+        }
+    }
+}
