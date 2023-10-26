79ddad4b203a1f7716d97749bb2833c50f014508
GoshoPetrov <yankrustev@gmail.com>
2023-10-26 18:02:58 +0300

    Problem 2 methods

---
+++ Sign of Integer Numbers/Sign of Integer Numbers/Sign of Integer Numbers.cs
@@ -0,0 +1,28 @@
+﻿namespace Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The number {num} is {Sign(num)}.");
+        }
+
+        static string Sign(int num)
+        {
+            if (num > 0)
+            {
+                return "positive";
+            }
+            else if (num < 0)
+            {
+                return "negative";
+            }
+            else
+            {
+                return "zero";
+            }
+        }
+    }
+}
