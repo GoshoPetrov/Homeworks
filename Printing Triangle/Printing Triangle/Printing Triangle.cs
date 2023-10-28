541cb58d05f473239a78e6ac05208d6cc0bcc74c
GoshoPetrov <yankrustev@gmail.com>
2023-10-28 10:39:19 +0300

    Problem 4 methods - Printing Triangle

---
+++ Printing Triangle/Printing Triangle/Printing Triangle.cs
@@ -0,0 +1,32 @@
+﻿namespace Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintTriangle(num);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for(int j = 0; j <= n; j++)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write($"{i + 1} ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int j = n - 1; j > 0; j--)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write($"{i + 1} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
