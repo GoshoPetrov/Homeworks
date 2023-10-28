8b1999779fd733dc9414e313c926fe3844d64cd3
GoshoPetrov <yankrustev@gmail.com>
2023-10-28 10:59:43 +0300

    Problems 5 and 6 methods

---
+++ Repeat String/Repeat String/Repeat String.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(word, count));
+        }
+
+        static string RepeatString(string word, int count)
+        {
+            string newWord = null;
+            for(int i = 0; i < count; i++)
+            {
+                newWord += word;
+            }
+            return newWord;
+        }
+    }
+}
