c781393e645c12f6e74d5dc687623fe3fb84f890
GoshoPetrov <yankrustev@gmail.com>
2023-10-28 11:19:23 +0300

    Problem 7 - Math Power

---
+++ Math Power/Math Power/Math Power.cs
@@ -0,0 +1,21 @@
+﻿namespace Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathPower(num, power));
+        }
+        static double MathPower(double num, double power)
+        {
+            double result = 0;
+
+            result = Math.Pow(num, power);
+
+            return result;
+        }
+    }
+}
