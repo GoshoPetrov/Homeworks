f9b4c5a765b722eb204f0d5e4cc2608163eeaa7b
GoshoPetrov <yankrustev@gmail.com>
2023-10-29 11:27:54 +0200

    Proble 10 methods - Math operations

---
+++ Math operations/Math operations/Math operations.cs
@@ -0,0 +1,40 @@
+﻿namespace Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOne = double.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            double numTwo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(Calculate(numOne, type, numTwo)));
+
+
+        }
+
+        static double Calculate(double numOne, string type, double numTwo)
+        {
+            double result = 0;
+
+            if(type == "/")
+            {
+                result = numOne / numTwo;
+            }
+            else if(type == "*")
+            {
+                result = numOne * numTwo;
+            }
+            else if(type == "+")
+            {
+                result = numOne + numTwo;
+            }
+            else if(type == "-")
+            {
+                result = numOne - numTwo;
+            }
+
+            return result;
+        }
+    }
+}
