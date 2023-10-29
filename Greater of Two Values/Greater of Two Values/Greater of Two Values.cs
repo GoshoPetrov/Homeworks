103e9e29d3b4e102dd7008232fea22b47023c715
GoshoPetrov <yankrustev@gmail.com>
2023-10-29 10:40:24 +0200

    Problem 8 methods - Greater of Two Values

---
+++ Greater of Two Values/Greater of Two Values/Greater of Two Values.cs
@@ -0,0 +1,48 @@
+﻿namespace Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            if (type == "int")
+            {
+                Console.WriteLine(GetMax(int.Parse(a), int.Parse(b)));
+            }
+
+            if (type == "char")
+            {
+                Console.WriteLine(GetMax(a[0], b[0]));
+            }
+
+            if (type == "string")
+            {
+                Console.WriteLine(GetMax(a, b));
+            }
+
+        }
+
+        
+        static int GetMax(int a, int b)
+        {
+            if (a > b) return a;
+            return b;
+        }
+
+        static char GetMax(char a, char b)
+        {
+            if (a > b) return a;
+            return b;
+        }
+
+        static string GetMax(string a, string b)
+        {
+            if (string.Compare(a, b) > 0) return a;
+            return b;
+        }
+
+    }
+}
