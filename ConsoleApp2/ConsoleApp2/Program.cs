71839e0ce698018b4362c75e4639f0138a007acb
GoshoPetrov <yankrustev@gmail.com>
2023-11-08 18:23:59 +0200

    Problem 4 methods exercise

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = '5';
+            char b = 'a';
+
+            Console.WriteLine(Math.Min(a, b));
+
+        }
+    }
+}
---
+++ Password Validator/Password Validator/Password Validator.cs
@@ -0,0 +1,74 @@
+﻿namespace Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            CheckCount(password);
+        }
+
+        
+        static void CheckCount(string password)
+        {
+            if(password.Length < 6 || password.Length > 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                CheckOnlyDigitsAndLetters(password);
+            }
+            else
+            {
+                CheckOnlyDigitsAndLetters(password);
+            }
+        }
+
+        static void CheckOnlyDigitsAndLetters(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLetterOrDigit(password[i]))
+                {
+
+                }
+                else
+                {
+                    count++;
+                }
+            }
+                
+                if(count == 0)
+                {
+                    CheckAtLeastTwoDigits(password);
+                }
+                else
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                    CheckAtLeastTwoDigits(password);
+                }
+                
+            
+        }
+
+        static void CheckAtLeastTwoDigits(string password)
+        {
+            int countDigit = 0;
+            for(int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                {
+                    countDigit++;
+                }
+            }
+
+            if(countDigit < 2)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            else
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+    }
+}
