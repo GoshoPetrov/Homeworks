2e4080aca555a66ba669f72bb806656fac30e334
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2024-09-29 11:31:21 +0300

    Fractions

---
+++ Calculation with Fractions/Calculation with Fractions/Calculation with Fractions.cs
@@ -0,0 +1,34 @@
+﻿using Fractions;
+
+namespace Calculation_with_Fractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Fraction f1 = p(input[0]);
+            Fraction f2 = p(input[2]);
+
+            if (input[1] == "+")
+            {
+                Console.WriteLine($"{f1} + {f2} = {f1 + f2}");
+            } else
+            {
+                Console.WriteLine($"{f1} - {f2} = {f1 - f2}");
+
+            }
+        }
+
+        static Fraction p(string expession)
+        {
+            string[] f = expession.Split('/');
+
+            Fraction f1 = int.Parse(f[0]);
+            Fraction f2 = int.Parse(f[1]);
+
+            return f1 / f2;
+        }
+    }
+}
