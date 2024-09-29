9b624376098f414fc695e8d8aaf6a673caae1eb6
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2024-09-29 12:30:18 +0300

    Random Cards

---
+++ Randomize Cards/Randomize Cards/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomize_Cards
+{
+    internal class Card
+    {
+        public string Face { get; set; }
+
+        public string Suite { get; set; }
+
+        public Card(string fase, string suite)
+        {
+            this.Face = fase;
+            this.Suite = suite;
+        }
+        public string Print()
+        {
+            return $"{Face} {Suite}";
+        }
+    }
+}
---
+++ Randomize Cards/Randomize Cards/Randomize Cards.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace Randomize_Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] faces = new string[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            string[] suits = new string[] {"Spades", "Diamonds", "Clubs", "Hearts" };
+
+            List<Card> all = new List<Card>();
+            foreach (var f in faces)
+            {
+                foreach (var s in suits)
+                {
+                    all.Add(new Card(f, s));
+                }
+            }
+
+            var rnd = new Random();
+            while (all.Count > 0)
+            {
+                int randomIndex = rnd.Next(0, all.Count);
+                Console.WriteLine(all[randomIndex].Print());
+                all.RemoveAt(randomIndex);
+            }
+            
+        }
+    }
+}
