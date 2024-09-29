9402d7621efd9c7cd10cf8f1508b5422beb0c1c0
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2024-09-29 11:05:41 +0300

    Dates

---
+++ Dates/Dates/DateCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dates
+{
+    internal class DateCounter
+    { 
+        public int CalculateDifference(string date1, string date2)
+        {
+            DateTime Sdate1 = DateTime.Parse(date1);
+            DateTime Sdate2 = DateTime.Parse(date2);
+
+            TimeSpan diff = Sdate2 - Sdate1;
+            return Math.Abs(diff.Days);
+
+        }
+    }
+}
---
+++ Dates/Dates/Dates.cs
@@ -0,0 +1,17 @@
+﻿namespace Dates
+{
+    internal class Dates 
+    {
+        static void Main(string[] args)
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+
+
+            DateCounter dates = new DateCounter();
+
+            Console.WriteLine(dates.CalculateDifference(date1, date2));
+
+        }
+    }
+}
