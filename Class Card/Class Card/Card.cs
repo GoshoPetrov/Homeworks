d29420acda6104ae0ac90711a79b3e14bc1c42d3
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2024-09-29 12:05:32 +0300

    Cards

---
+++ Class Card/Class Card/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Card
+{
+    internal class Card
+    {
+        public string Face { get; set; } // 8
+
+        public string Suite { get; set; } // Spades
+
+        public Card(string fase, string suite)
+        {
+            this.Face = fase;
+            this.Suite = suite;
+        }
+        public string Print()
+        {
+            return $"{Face} {Suite}";
+        }
+    }
+}
---
+++ Class Card/Class Card/Class Card.cs
@@ -0,0 +1,21 @@
+﻿namespace Class_Card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> list = new List<Card>
+            {
+                new Card("A", "Spades"),
+                new Card("J", "Diamond"),
+                new Card("Q", "Clubs"),
+                new Card("10", "Hearts")
+            };
+
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.Print());
+            }
+        }
+    }
+}
